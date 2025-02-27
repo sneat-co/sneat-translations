5af5c5b3154af123e9c98f44732340a803d0fa02
Alexander Trakhimenok <alex@trakhimenok.com>
2025-02-27 18:08:53 +0000

    fix: trackus translations

---
+++ trans/consts_trackus.go
@@ -0,0 +1,38 @@
+package trans
+
+const (
+	TrackerPushUps       = "push_ups"
+	TrackerPullUps       = "pull_ups"
+	TrackerSquats        = "squats"
+	TrackerJumpingJacks  = "jumping_jacks"
+	TrackerWeight        = "weight"
+	TrackerHeight        = "height"
+	TrackerBloodPressure = "blood_pressure"
+	TrackerFuelVolume    = "fuel_volume"
+	TrackerFuelCost      = "fuel_cost"
+	TrackerMileage       = "mileage"
+	TrackerSpending      = "spending"
+
+	TrackerCategoryHealth  = "health"
+	TrackerCategoryFitness = "fitness"
+	TrackerCategoryVehicle = "vehicle"
+
+	CustomTracker = "custom_tracker"
+
+	BackToTrackers = "back_to_trackers"
+
+	AddTracker         = "add_tracker"
+	ShareTracker       = "share_tracker"
+	RecordTrackerValue = "record_tracker_value"
+	FamilyTrackers     = "family_trackers"
+
+	NoActiveTrackers = "no_active_trackers"
+	OneActiveTracker = "one_active_tracker"
+	NActiveTrackers  = "n_active_trackers"
+
+	HintForIntTracker = "hint_for_int_tracker"
+
+	HintToShareTracker = "hint_to_share_tracker"
+
+	Tracker = "tracker"
+)
