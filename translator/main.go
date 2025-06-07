d0e4a83bc01d798f8bd778a3e42aa722a6b1ce2b
Alexander Trakhimenok <alex@trakhimenok.com>
2025-06-07 14:21:32 +0100

    fix: 3 more translations

--- translator/main.go
+++ translator/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	/* To authenticate with Google Cloud, run:
+
+	gcloud auth application-default login
+
+	*/
 	if err := translateMissing("en-UK"); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 	}
